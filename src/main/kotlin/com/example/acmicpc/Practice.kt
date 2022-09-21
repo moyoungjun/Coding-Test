e99a428f96a68f663615237ac24052f4cdf7500b
moyoungjun <mo1212123@naver.com>
2022-09-21 14:49:35 +0900

    d

--- src/main/kotlin/com/example/acmicpc/Practice.kt
+++ src/main/kotlin/com/example/acmicpc/Practice.kt
@@ -9,5 +9,5 @@ fun main(){
         val s = br.readLine()
         println("${s::class.simpleName}")
         val d = br.readLine().toInt()
-        println("${d::class.simpleName}")
+        println("${d::class.simpleName}.")
 }
